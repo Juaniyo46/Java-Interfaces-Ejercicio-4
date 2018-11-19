118a0c6c9e3428dfcc1bb3d1067122ec5980f74a
Juan <juan.alvarez@escuelaestech.es>
2018-11-19 12:04:22 +0100

    EN PROCESO 1

---
+++ src/es/JuanAntonio/Math3.java
@@ -0,0 +1,28 @@
+package es.JuanAntonio;
+
+public class Math3 implements Extremos{
+
+    @Override
+    public double max(double[] a) {
+
+        double decimalMax =
+        for ()
+
+        return 0;
+    }
+
+    @Override
+    public double min(double[] a) {
+        return 0;
+    }
+
+    @Override
+    public int max(int[] a) {
+        return 0;
+    }
+
+    @Override
+    public int min(int[] a) {
+        return 0;
+    }
+}
